cfbc3df733522b2cfe6ec757dea1c14ba4211df9
meiyou10 <meiyou10@outlook.com>
2017-02-16 22:54:57 -0700

    Initial commit of Eclipse Maven project to test Travis

---
+++ HelloWorldMavenTravis/src/helloWorldMavenTravis/MainHelloWorldMavenTravis.java
@@ -0,0 +1,12 @@
+package helloWorldMavenTravis;
+
+public class MainHelloWorldMavenTravis
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World Maven Travis!");
+
+	}
+
+}
