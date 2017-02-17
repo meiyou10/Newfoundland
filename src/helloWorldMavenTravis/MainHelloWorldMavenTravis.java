80dfe2ed32c906e14c19737ad2d53107c15ecb83
meiyou10 <meiyou10@users.noreply.github.com>
2017-02-17 00:27:04 -0700

    Successful build!

--- src/helloWorldMavenTravis/MainHelloWorldMavenTravis.java
+++ src/helloWorldMavenTravis/MainHelloWorldMavenTravis.java
@@ -6,6 +6,8 @@ public class MainHelloWorldMavenTravis
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World Maven Travis!");
+		
+		System.out.println("Successful build!");
 
 	}
 
